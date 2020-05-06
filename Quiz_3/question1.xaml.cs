3ef0175c3c754be4d668b39fd5f418f55d381341
Zhanzhou Ta <6151957@johnabbottcollege.net>
2020-05-06 18:56:09 -0400

    first

---
+++ Quiz_3/question1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quiz_3
+{
+    /// <summary>
+    /// Interaction logic for question1.xaml
+    /// </summary>
+    public partial class question1 : Window
+    {
+        IList<Student> studentList = new List<Student>();
+
+        public question1()
+        {
+            InitializeComponent();
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Student stu = new Student
+                (
+                Int32.Parse(tbxId.Text),
+                tbxFName.Text,
+                tbxLName.Text,
+                Checkregisterd.IsChecked ?? false
+                );
+            studentList.Add(stu);
+            int num = studentList.Count();
+
+            MessageBox.Show($"There are {num.ToString()} added students");
+
+
+        }
+    }
+}
---
+++ Quiz_3/question2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quiz_3
+{
+    /// <summary>
+    /// Interaction logic for question2.xaml
+    /// </summary>
+    public partial class question2 : Window
+    {
+        question1 q1 = new question1();
+        public question2()
+        {
+            InitializeComponent();
+            
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            dataGrid.ItemsSource = q1.
+        }
+    }
+}
